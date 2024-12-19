21d9bee5273c87ec491a4908a8aff6af4b893e1d
Callum Gare <callum@gare.au>
2024-12-19 20:17:48 +1100

    feat: make secrets sets config file optional

--- src/lib/secrets.ts
+++ src/lib/secrets.ts
@@ -2,13 +2,33 @@ import fs from "node:fs/promises";
 
 let cachedSecretSets: Record<string, Record<string, unknown>>;
 
-export async function getSecretsSets(
-	secretSetsConfigPath = "./.secrets-sets.json",
-) {
+export async function getSecretsSets() {
+	const defaultSecretSetsConfigPath = "./.secrets-sets.json";
+	const customSecretSetsConfigPath = process.env.SECRETS_SETS_CONFIG_PATH;
+
+	const secretSetsConfigPath =
+		customSecretSetsConfigPath ?? defaultSecretSetsConfigPath;
+
 	if (!cachedSecretSets) {
-		cachedSecretSets = JSON.parse(
-			await fs.readFile(secretSetsConfigPath, "utf8"),
-		);
+		let configString: string;
+		try {
+			configString = await fs.readFile(secretSetsConfigPath, "utf8");
+		} catch (error) {
+			if (customSecretSetsConfigPath) {
+				console.warn(
+					`Secrets sets config path set to ${secretSetsConfigPath} but file not found or accessible.`,
+				);
+			}
+			configString = "{}";
+		}
+		try {
+			cachedSecretSets = JSON.parse(configString);
+		} catch (error) {
+			console.warn(
+				`Secrets sets config path set to ${secretSetsConfigPath} but file is not valid JSON.`,
+			);
+			cachedSecretSets = {};
+		}
 	}
 	return cachedSecretSets;
 }
